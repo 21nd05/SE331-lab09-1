7f1e27e8ca9d363fe33e0ad695e61769adb988fa
Mojune <viparat_u@cmu.ac.th>
2016-04-18 15:05:56 +0700

    1.14

---
+++ app/src/app/shoppingcart/shoppingCartService.js
@@ -0,0 +1,13 @@
+(function(){
+
+  'use strict'
+  var shoppingCartServices = angular.module('shoppingCartServices',['ngResource']);
+  shoppingCartServices.factory('shoppingCartService',function($resource){
+    return $resource('http://localhost:8080/shoppingcart/:id', { id: '@_id' }, {
+      update: {
+        method: 'PUT' // this method issues a PUT request
+      }});
+
+  })
+
+})();
